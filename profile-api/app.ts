6384a83c308e8bb7e194b44fda764445996897bb
pom chanchai <pom51142@gmail.com>
2024-02-29 17:33:51 +0700

    add controller

--- profile-api/app.ts
+++ profile-api/app.ts
@@ -1,9 +1,16 @@
-import express from "express";
+import express, { Response } from "express"
+import { profile } from "./profile-controller"
 
 const app = express()
 
 app.use(express.json())
 
+app.use("/profile",profile)
+
+app.use("*", (_, res: Response) => {
+    res.status(404).type("text/plain").send("page not found")
+})
+
 app.listen(3000, () => {
-    console.log(`Example app listening on port 3000`)
+    console.log("profile app listening on port 3000")
 })
