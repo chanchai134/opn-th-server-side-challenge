4b680ea2856da1302bbc9bd298a3c81a2c8634e1
pom chanchai <pom51142@gmail.com>
2024-02-29 17:45:06 +0700

    promote member to class

--- profile-api/models/member.ts
+++ profile-api/models/member.ts
@@ -1,12 +1,35 @@
 import { Address } from "./address"
 import { Gender } from "./gender"
 
-export interface Member {
-    email: string,
-    password: string,
-    name: string,
-    dateOfBirth: string,
-    gender: Gender,
-    address: Address,
+export interface MemberDTO {
+    email: string
+    password: string
+    name: string
+    dateOfBirth: string
+    gender: Gender
+    address: Address
     subscribeNewsletter: boolean
+}
+export class Member {
+    constructor(
+        public email: string,
+        public password: string,
+        public name: string,
+        public dateOfBirth: string,
+        public gender: Gender,
+        public address: Address,
+        public subscribeNewsletter: boolean
+    ) {}
+
+    static create(dto: MemberDTO): Member {
+        return new Member(
+            dto.email,
+            dto.password,
+            dto.name,
+            dto.dateOfBirth,
+            dto.gender,
+            dto.address,
+            dto.subscribeNewsletter
+        )
+    }
 }
